88036b6853ac55a3f0479e442b04caca89df81e2
xu.qinqin <18333617650@163.com>
2019-09-11 09:33:02 +0800

    php-fpm env config

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ if [ false == $exists ]; then
 fi
 
 echo "current project_path: $project_path"
-docker exec -it $1 /bin/bash -c "cd $project_path && $2 $3 $4 $5 $6"
+docker exec -it $1 /bin/bash -c "cd $project_path && $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19}"
