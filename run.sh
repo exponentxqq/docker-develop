7e40a890ab6f1ef6e060c2c512e28d291643bb8f
xu.qinqin <18333617650@163.com>
2020-04-21 09:57:33 +0800

    add maven to java container

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 project_path=$(pwd)
-container_names=`cd /develop/docker && docker-compose ps | awk '{print $1}' | grep -v Name | grep -v "^-"`
+container_names=`cd /develop/docker && docker-compose ps | grep -v Exit | awk '{print $1}' | grep -v Name | grep -v "^-"`
 exists=false
 for container_name in $container_names
 do
-    if [[ $1 == $container_name ]]; then
+    if [ $1 == $container_name ]; then
         exists=true
     fi
 done
 
 if [ false == $exists ]; then
-    echo "$1 container_name not exists"
-    echo "current containers: $container_names"
+    echo "$1 container not exists"
+    echo "current containers:"
+    echo $container_names
     exit 1;
 fi
 
@@ -18,7 +19,7 @@ echo "[$1]current project_path: $project_path"
 command=$*
 command=${command#* }
 
-if [[ $1 == "redis" ]]; then
+if [ $1 == "redis" ]; then
     docker exec -it $1 /bin/bash -c "$command"
 elif [[ $1 == "workspace" ]]; then
     docker exec -it $1 /bin/zsh -c "[ -d $project_path ] && (cd $project_path && $command) || $command"
