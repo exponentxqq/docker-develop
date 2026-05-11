6ba82fd78fde9de6cad98c9c8c66633ca6e358f8
xu.qinqin <wz.xu.qinqin@gmail.com>
2026-05-11 15:36:57 +0800

    update docker run

--- run.sh
+++ run.sh
@@ -1,31 +1,30 @@
 DOCKER_ROOT_DIR=$(dirname $(readlink $0))
-source $DOCKER_ROOT_DIR/.env
+source "$DOCKER_ROOT_DIR/.env"
 project_path=$(pwd)
 project_path=${project_path/#$HOST_PROJECT_PATH/$CONTAINER_PROJECT_PATH}
 
 container_names=$(cd $DOCKER_ROOT_DIR && docker compose ps | grep -v Exit | grep -v exited | awk '{print $1}' | grep -v Name | grep -v NAME | grep -v "^-")
 exists=false
-for container_name in $container_names
-do
-    if [ "$1" == "$container_name" ]; then
-        exists=true
-    fi
+for container_name in $container_names; do
+  if [ "$1" == "$container_name" ]; then
+    exists=true
+  fi
 done
 
 if [ false == $exists ]; then
-    echo "start container[$1]..."
-    cd $DOCKER_ROOT_DIR && docker compose up -d $1
+  echo "start container[$1]..."
+  cd $DOCKER_ROOT_DIR && docker compose up -d $1
 fi
 
 echo "[$1]current project_path: $project_path"
 command=$*
-echo $command
+echo "$command"
 command=${command#* }
 
 if [ "$1" == "redis" ]; then
-    docker exec -it "$1" /bin/bash -c "$command"
+  exec docker exec -it "$1" /bin/bash -c "$command"
 elif [ "$1" == "workspace" ]; then
-    docker exec -it "$1" /bin/zsh -c "[ -d $project_path ] && (cd $project_path && $command) || $command"
+  exec docker exec -it "$1" /bin/zsh -c "[ -d $project_path ] && (cd $project_path && $command) || $command"
 else
-    docker exec -it "$1" /bin/bash -c "[ -d $project_path ] && (cd $project_path && $command) || $command"
+  exec docker exec -it "$1" /bin/bash -c "[ -d $project_path ] && (cd $project_path && $command) || $command"
 fi
